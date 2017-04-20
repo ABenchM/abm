64f42c14e90870314080f51d03f989ebd322d3a0
Henrik Niehaus <henrik.niehaus@gmx.de>
2017-04-20 15:47:41 +0200

    Make sure to close the repository for each operation, so that all file handles get released.

--- bnd-workspace/de.fraunhofer.abm.scm.git/src/de/fraunhofer/abm/scm/git/Git.java
+++ bnd-workspace/de.fraunhofer.abm.scm.git/src/de/fraunhofer/abm/scm/git/Git.java
@@ -18,33 +18,43 @@ public class Git implements SCM {
     @Override
     public String clone(RepositoryDTO repo, File targetDir) throws Exception {
         CloneCommand cloneCommand = new CloneCommand().setURI(repo.repositoryUrl).setDirectory(targetDir);
-        cloneCommand.call();
+        cloneCommand.call().close();
 
         Repository repository = new RepositoryBuilder().setWorkTree(targetDir).build();
-        ObjectId headRef = repository.resolve("HEAD");
-        return headRef.getName();
+        try {
+            ObjectId headRef = repository.resolve("HEAD");
+            String name = headRef.getName();
+            return name;
+        } finally {
+            repository.close();
+        }
     }
 
     @Override
     public void checkout(RepositoryDTO repo, File repoDir, String commitSha) throws Exception {
         Repository repository = new RepositoryBuilder().setWorkTree(repoDir).build();
-        org.eclipse.jgit.api.Git git = new org.eclipse.jgit.api.Git(repository);
         try {
+            org.eclipse.jgit.api.Git git = new org.eclipse.jgit.api.Git(repository);
             CheckoutCommand cmd = git.checkout().setName(commitSha);
             cmd.call();
-        } finally {
             git.close();
+        } finally {
+            repository.close();
         }
     }
 
     @Override
     public String update(RepositoryDTO repo, File repoDir) throws Exception {
         Repository repository = new RepositoryBuilder().setWorkTree(repoDir).build();
-        org.eclipse.jgit.api.Git git = new org.eclipse.jgit.api.Git(repository);
-        git.pull();
-        git.close();
-        ObjectId headRef = repository.resolve("HEAD");
-        return headRef.getName();
+        try {
+            org.eclipse.jgit.api.Git git = new org.eclipse.jgit.api.Git(repository);
+            git.pull();
+            git.close();
+            ObjectId headRef = repository.resolve("HEAD");
+            return headRef.getName();
+        } finally {
+            repository.close();
+        }
     }
 
     @Override
