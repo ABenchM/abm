872f7f72c767e727a579515477e4b9265f80519d
Henrik Niehaus <henrik.niehaus@gmx.de>
2017-04-26 18:20:32 +0200

    Pass on the environment variables of the JVM to the docker command line tool

--- bnd-workspace/de.fraunhofer.abm.builder.docker.base/src/de/fraunhofer/abm/builder/docker/base/AbstractDockerStep.java
+++ bnd-workspace/de.fraunhofer.abm.builder.docker.base/src/de/fraunhofer/abm/builder/docker/base/AbstractDockerStep.java
@@ -3,6 +3,8 @@ package de.fraunhofer.abm.builder.docker.base;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ExecutorService;
 
 import org.slf4j.Logger;
@@ -25,7 +27,7 @@ public abstract class AbstractDockerStep<T> extends AbstractBuildStep<T> {
 
     protected Result exec(String cmd, File dir) throws IOException, InterruptedException {
         logger.debug("Executing command [{}] in directory {}", cmd, dir.getAbsolutePath());
-        String[] env = {};
+        String[] env = environmentToArray();
         String[] _cmd = cmd.split(" ");
         Process p = Runtime.getRuntime().exec(_cmd, env, dir);
         ByteArrayOutputStream stdout = new ByteArrayOutputStream();
@@ -43,5 +45,16 @@ public abstract class AbstractDockerStep<T> extends AbstractBuildStep<T> {
         public int exitValue;
         public String stdout = "";
         public String stderr = "";
+    }
+
+    public static String[] environmentToArray() {
+        Map<String,String> env = System.getenv();
+        String[] envArray = new String[env.size()];
+        int index = 0;
+        for (Entry<String,String> entry : env.entrySet()) {
+            String arrayEntry = entry.getKey() + '=' + entry.getValue();
+            envArray[index++] = arrayEntry;
+        }
+        return envArray;
     }
 }
