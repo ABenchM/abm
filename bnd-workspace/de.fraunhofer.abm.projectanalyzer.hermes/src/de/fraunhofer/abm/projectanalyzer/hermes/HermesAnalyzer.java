51aaa6c97480a7f74d8e29995b8bbb88090f0b59
Henrik Niehaus <henrik.niehaus@gmx.de>
2017-04-27 21:37:34 +0200

    new analyzer module for hermes

---
+++ bnd-workspace/de.fraunhofer.abm.projectanalyzer.hermes/src/de/fraunhofer/abm/projectanalyzer/hermes/HermesAnalyzer.java
@@ -0,0 +1,19 @@
+package de.fraunhofer.abm.projectanalyzer.hermes;
+
+import java.io.File;
+import java.util.List;
+
+import de.fraunhofer.abm.domain.RepositoryDTO;
+import de.fraunhofer.abm.domain.RepositoryPropertyDTO;
+import de.fraunhofer.abm.projectanalyzer.api.ProjectAnalyzer;
+
+
+public class HermesAnalyzer implements ProjectAnalyzer {
+
+    @Override
+    public List<RepositoryPropertyDTO> analyze(RepositoryDTO repo, File directory) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
