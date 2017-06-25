462e4da53b4b31ade27d5f5e39c85a93b5d9750c
ankur-1989 <ankur1989.23@gmail.com>
2017-06-25 13:22:00 +0200

    Update AbstractDockerStep.java

--- bnd-workspace/de.fraunhofer.abm.builder.docker.base/src/de/fraunhofer/abm/builder/docker/base/AbstractDockerStep.java
+++ bnd-workspace/de.fraunhofer.abm.builder.docker.base/src/de/fraunhofer/abm/builder/docker/base/AbstractDockerStep.java
@@ -27,13 +27,7 @@ public abstract class AbstractDockerStep<T> extends AbstractBuildStep<T> {
 
     protected Result exec(String cmd, File dir) throws IOException, InterruptedException {
         logger.debug("Executing command [{}] in directory {}", cmd, dir.getAbsolutePath());
-        String[] env = {"DOCKER_CERT_PATH=C:/Users/ankur/.docker/machine/certs",
-        		"DOCKER_HOST=tcp://192.168.99.100:2376",
-        		"DOCKER_MACHINE_NAME=default",
-        		"DOCKER_TLS_VERIFY=1", 
-        		"DOCKER_TOOLBOX_INSTALL_PATH=C:/Program Files/Docker Toolbox"};
-        
-        //		environmentToArray();
+        String[] env = 	environmentToArray();
         String[] _cmd = cmd.split(" ");
         Process p = Runtime.getRuntime().exec(_cmd, env, dir);
         ByteArrayOutputStream stdout = new ByteArrayOutputStream();
