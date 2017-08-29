386c8373ce91d7b545d45fc634e8a236ba8dd290
ankur-1989 <ankur1989.23@gmail.com>
2017-08-29 23:43:14 +0200

    Update FilterStatusDTO.java

--- bnd-workspace/de.fraunhofer.abm.domain/src/de/fraunhofer/abm/domain/FilterStatusDTO.java
+++ bnd-workspace/de.fraunhofer.abm.domain/src/de/fraunhofer/abm/domain/FilterStatusDTO.java
@@ -2,7 +2,7 @@ package de.fraunhofer.abm.domain;
 
 public class FilterStatusDTO {
 
-  public String id;	
+  public String id = UUID.randomUUID().toString();;	
   public String filtername;
   public boolean activate;
   public String versionid;
