1d070b43e3796a912ae78d6b98f7d56dc6eebae6
mac24079 <mac24079@mail.uni-paderborn.de>
2018-06-03 18:35:55 +0200

    issue-127 fix

--- bnd-workspace/de.fraunhofer.abm.app/web/filters/SiteNameFilter.js
+++ bnd-workspace/de.fraunhofer.abm.app/web/filters/SiteNameFilter.js
@@ -1,7 +1,7 @@
 angular.module('de.fraunhofer.abm').filter('siteName', function(){
 	return function(input){
 		if(input.includes("github.com")){return 'Github';}
-		else if(input.includes("bitbucket.com")){return 'Bitbucket';}
+		else if(input.includes("bitbucket.org")){return 'Bitbucket';}
 		else if(input.includes("maven.com")){return 'Maven';}
 		else{return 'Unknown';}
 	};
