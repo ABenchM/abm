0d341fd507fdec7d5914b9cfb94cdf163e63636f
ankur-1989 <ankur1989.23@gmail.com>
2018-06-08 00:00:02 +0200

    Update abm.sql
    Added new column in the user table.

--- docs/abm.sql
+++ docs/abm.sql
@@ -160,5 +160,6 @@ CREATE TABLE `query_feature_map` (
 CREATE TABLE `user` (
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `approved` tinyint(4) DEFAULT NULL
+  `approved` tinyint(4) DEFAULT NULL,
+  `approval_token` varchar(50) DEFAULT NULL
 );
