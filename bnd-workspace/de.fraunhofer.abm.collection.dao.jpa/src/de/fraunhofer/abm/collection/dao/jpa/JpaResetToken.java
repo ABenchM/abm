835664b3f55f35622fb4e8dc41728c9352bcaf33
anitha181992 <l.anitha.18@gmail.com>
2018-08-25 11:06:06 +0200

    reset token jpa changes

---
+++ bnd-workspace/de.fraunhofer.abm.collection.dao.jpa/src/de/fraunhofer/abm/collection/dao/jpa/JpaResetToken.java
@@ -0,0 +1,33 @@
+package de.fraunhofer.abm.collection.dao.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import de.fraunhofer.abm.domain.ResetTokenDTO;;
+
+@Entity(name = "reset_token")
+public class JpaResetToken {
+	
+	@Id
+	@Column
+	public String username;
+
+	@Column
+	public String token;
+	
+	
+	public static JpaResetToken fromDTO(ResetTokenDTO dto) {
+		JpaResetToken jpa = new JpaResetToken();
+		jpa.username = dto.username;
+		jpa.token = dto.token;
+		return jpa;
+	}
+
+	public ResetTokenDTO toDTO() {
+		ResetTokenDTO dto = new ResetTokenDTO();
+		dto.username = this.username;
+		dto.token = this.token;
+		return dto;
+	}
+}
