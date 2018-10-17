5bd2f0c1ac1583c6e0d25876904413e300216194
Chinmay <cppingale@gmail.com>
2018-10-17 18:12:45 +0200

    deletePublicCollection API now accepts list of public collections
    deletePublicCollection API now accepts list of public collections

--- bnd-workspace/de.fraunhofer.abm.app/src/de/fraunhofer/abm/app/controllers/DeletePublicCollectionController.java
+++ bnd-workspace/de.fraunhofer.abm.app/src/de/fraunhofer/abm/app/controllers/DeletePublicCollectionController.java
@@ -1,7 +1,7 @@
 package de.fraunhofer.abm.app.controllers;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -12,6 +12,7 @@ import de.fraunhofer.abm.app.auth.Authorizer;
 import de.fraunhofer.abm.collection.dao.CollectionDao;
 import osgi.enroute.configurer.api.RequireConfigurerExtender;
 import osgi.enroute.rest.api.REST;
+import osgi.enroute.rest.api.RESTRequest;
 import osgi.enroute.webserver.capabilities.RequireWebServerExtender;
 
 @RequireWebServerExtender
@@ -26,17 +27,23 @@ public class DeletePublicCollectionController extends AbstractController impleme
     @Reference
     private CollectionDao collectionDao;
     
+    interface CollectionRequest extends RESTRequest {
+        Map<String, String> _body();
+	}
    
-    public boolean deleteDeletepubliccollection(String id) throws IOException{
-    	ArrayList<String> users = new ArrayList<String>();
-    	users.add("Admin");
-    	users.add("UserAdmin");
-    	authorizer.requireRoles(users);
-    	//id = "3b847907-b97f-481e-a1b4-fea328a9d3ee";
-        
-        logger.debug("deleting collection with"+id);
-    	// delete the collection
-    	collectionDao.delete(id);
+    public boolean deleteDeletepubliccollection(CollectionRequest cr) throws IOException{
+    	authorizer.requireRole("UserAdmin");
+		Map<String, String> params = cr._body();
+		String[] deleteCollection = params.get("deleteCollections").split(",");
+		for (String id: deleteCollection) {
+	        try {
+        	    logger.debug("deleting collection with"+id);
+            	// delete the collection
+            	collectionDao.delete(id);
+	        } catch (Exception e) {
+	        	logger.info("Exception");
+	        }
+	    }
         return true;
     }
 
