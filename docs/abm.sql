8fa9dfa05ee94aebe4bade214fd34ecc8b1f14f7
Anu <anutonythottam@gmail.com>
2018-11-20 00:46:54 +0100

    Update abm.sql
    user table changes

--- docs/abm.sql
+++ docs/abm.sql
@@ -159,8 +159,14 @@ CREATE TABLE `query_feature_map` (
 );
 
 CREATE TABLE `user` (
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `affiliation` varchar(255) NOT NULL,
   `approved` tinyint(4) DEFAULT NULL,
-  `approval_token` varchar(50) DEFAULT NULL
+  `approval_token` varchar(50) DEFAULT NULL,
+  `locked` tinyint(4) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+   PRIMARY KEY (`name`)
 );
