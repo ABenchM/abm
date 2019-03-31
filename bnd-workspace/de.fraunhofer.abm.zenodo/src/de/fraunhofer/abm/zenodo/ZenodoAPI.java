c004d3abb55481cd24f939df0016a29aff4d7271
Ankur Gupta <agupta@mail.uni-paderborn.de>
2019-03-31 20:25:27 +0200

    Zenodo part8

--- bnd-workspace/de.fraunhofer.abm.zenodo/src/de/fraunhofer/abm/zenodo/ZenodoAPI.java
+++ bnd-workspace/de.fraunhofer.abm.zenodo/src/de/fraunhofer/abm/zenodo/ZenodoAPI.java
@@ -1,7 +1,18 @@
 package de.fraunhofer.abm.zenodo;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface ZenodoAPI {
 
 	public boolean test();
+	public Deposition getDeposition(Integer id);
+	public List<Deposition> getDepositions();
+	public Deposition updateDeposition(Deposition deposition);
+	public void deleteDeposition(Integer id);
+	public Deposition createDeposition(final Metadata m) throws UnsupportedOperationException, IOException ;
+	public List<DepositionFile> getFiles(Integer depositionId);
+	public DepositionFile uploadFile(final FileMetadata f, Integer depositionId) throws UnsupportedOperationException, IOException;
+	public boolean discard(Integer id);
 	
 }
--- bnd-workspace/de.fraunhofer.abm.zenodo/src/de/fraunhofer/abm/zenodo/impl/ZenodoAPIImpl.java
+++ bnd-workspace/de.fraunhofer.abm.zenodo/src/de/fraunhofer/abm/zenodo/impl/ZenodoAPIImpl.java
@@ -1,22 +1,86 @@
 package de.fraunhofer.abm.zenodo.impl;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
 
+import de.fraunhofer.abm.zenodo.Deposition;
+import de.fraunhofer.abm.zenodo.DepositionFile;
+import de.fraunhofer.abm.zenodo.FileMetadata;
+import de.fraunhofer.abm.zenodo.Metadata;
 import de.fraunhofer.abm.zenodo.ZenodoAPI;
 
 @Component
 public class ZenodoAPIImpl implements ZenodoAPI {
 
 	
-	
+	static Map<String, String> header = new HashMap<>();
 	
 		
 	@Override
 	public boolean test() {
 		
 		
+		return false;
+	}
+
+
+	@Override
+	public Deposition getDeposition(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<Deposition> getDepositions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Deposition updateDeposition(Deposition deposition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void deleteDeposition(Integer id) {
+		// TODO Auto-generated method stub
 		
-		
+	}
+
+
+	@Override
+	public Deposition createDeposition(Metadata m) throws UnsupportedOperationException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public List<DepositionFile> getFiles(Integer depositionId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public DepositionFile uploadFile(FileMetadata f, Integer depositionId)
+			throws UnsupportedOperationException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public boolean discard(Integer id) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 	
